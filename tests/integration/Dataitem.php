8b32cff057b5b3b23b9855b0161169fb4aaf51e6
Mark Kimsal <metrofindings@gmail.com>
2014-10-17 14:56:26 -0400

    Add delete test and truncate intg table when done.

--- tests/integration/Dataitem.php
+++ tests/integration/Dataitem.php
@@ -7,7 +7,7 @@ class Metrodb_Tests_Integration_Dataitem extends PHPUnit_Framework_TestCase {
 
 
 	public function setUp() {
-		Metrodb_Connector::setDsn('default', 'mysql://root:mysql@localhost/metrodb_test');
+		Metrodb_Connector::setDsn('default', 'mysql://docker:mysql@192.168.2.65:3309/metrodb_test');
 	}
 
 	public function test_save_new_dataitem() {
@@ -23,5 +23,27 @@ class Metrodb_Tests_Integration_Dataitem extends PHPUnit_Framework_TestCase {
 		$listAnswer = $finder->findAsArray();
 
 		$this->assertEquals('value_a', $listAnswer[0]['column1']);
+	}
+
+	public function test_delete_dataitem() {
+		$di = new Metrodb_Dataitem('foo', 'foo_bar');
+
+		$di->column1 = 'value_a';
+		$x = $di->save();
+
+		$this->assertFalse(!$x);
+
+		$di->delete();
+
+		$finder = new Metrodb_Dataitem('foo', 'foo_bar');
+		$finder->andWhere('column1', 'value_a');
+		$listAnswer = $finder->findAsArray();
+
+		$this->assertEquals(0, count($listAnswer));
+	}
+
+	public function tearDown() {
+		$db = Metrodb_Connector::getHandle('default');
+		$db->execute('TRUNCATE `foo`');
 	}
 }
