9a2481e3b101eaa0fe1c3598b6f654431dc036d4
Mark Kimsal <metrofindings@gmail.com>
2014-12-01 17:32:27 -0500

    Add test for valuesAsArray

--- tests/Dataitem.php
+++ tests/Dataitem.php
@@ -8,7 +8,7 @@ class Metrodb_Tests_Dataitem extends PHPUnit_Framework_TestCase {
 	public function setUp() {
 	}
 
-	public function test_TableNamesandPkey() {
+	public function test_table_names_and_pkey() {
 		$di = new Metrodb_Dataitem('foo', 'foo_bar');
 
 		$this->assertEquals('foo',       $di->getKind());
@@ -16,13 +16,13 @@ class Metrodb_Tests_Dataitem extends PHPUnit_Framework_TestCase {
 		$this->assertEquals(NULL,        $di->getPrimaryKey());
 	}
 
-	public function test_IsNew() {
+	public function test_is_new() {
 		$di = new Metrodb_Dataitem('foo', 'foo_bar');
 
 		$this->assertTrue($di->_isNew);
 	}
 
-	public function test_EchoSelect() {
+	public function test_echo_select() {
 		$expectedString = "<pre>\nSELECT * FROM foo   where colA != 'bar'     LIMIT 125, 25 </pre>\n";
 		$di = new Metrodb_Dataitem('foo', 'foo_bar');
 
@@ -33,5 +33,20 @@ class Metrodb_Tests_Dataitem extends PHPUnit_Framework_TestCase {
 		$out = ob_get_contents();
 		ob_end_clean();
 		$this->assertEquals($expectedString, $out);
+	}
+
+
+	public function test_get_values_as_array() {
+		$di = new Metrodb_Dataitem('foo');
+		$di->set('colA', 90);
+		$di->colB =  'abc';
+
+		$expected = array(
+			'colA'=> 90,
+			'colB' => 'abc',
+			'foo_id' => NULL
+		);
+		$values = $di->valuesAsArray();
+		$this->assertEquals($expected, $values);
 	}
 }
