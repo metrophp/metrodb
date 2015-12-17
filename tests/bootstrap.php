2576f6f36fe528b38c9029aad1b7a4ae8fb1db85
Mark Kimsal <metrofindings@gmail.com>
2015-12-17 10:47:18 -0500

    Use mysqli as defautl test driver.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,4 +3,4 @@
 include_once('dataitem.php');
 include_once('connector.php');
 
-Metrodb_Connector::setDsn('default', 'mysql://root:mysql@localhost/metrodb_test');
+Metrodb_Connector::setDsn('default', 'mysqli://root:mysql@localhost/metrodb_test');
