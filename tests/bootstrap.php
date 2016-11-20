95a5aad54bf994e367fc2a631620c7193ef411e2
Mark Kimsal <metrofindings@gmail.com>
2016-11-20 09:32:15 -0500

    Add sqlite3 driver to testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,4 @@ include_once('dataitem.php');
 include_once('connector.php');
 
 Metrodb_Connector::setDsn('default', 'mysqli://root:mysql@localhost/metrodb_test');
+Metrodb_Connector::setDsn('sqlite3', 'sqlite3://localhost/metrodb_test');
